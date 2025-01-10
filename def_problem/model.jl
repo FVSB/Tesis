d2d5b9a962d21c21cf6e4a6412297044536e759d
Francisco Vicente Suárez Bellón <95106875+FVSB@users.noreply.github.com>
2025-01-10 12:49:45 -0500

    Refactor code problem gen

---
+++ def_problem/model.jl
@@ -0,0 +1,65 @@
+# Modulo para el problema
+using Symbolics
+
+include("structs/structs_declaration.jl")
+include("structs/restriction_types.jl")
+function Lower(opt_model::OptimizationModel)
+    return opt_model.follower_problem
+end
+
+function Upper(opt_model::OptimizationModel)
+    return opt_model.leader_problem
+end
+
+function SetObjectiveFunction(problem::Problem, expr::Symbolics.Num)
+    problem.expr=expr
+end
+
+
+function SetLeaderRestriction(model::OptimizationModel, expr::Symbolics.Num,
+    restriction_set_type::RestrictionSetType,
+    miu::Number
+    )
+    rest = LeaderRestrictionProblem(expr, miu, restriction_set_type)
+    push!(model.leaders_restriction, rest)
+   
+end
+
+function SetFollowerRestriction(
+    model::OptimizationModel,
+    expr::Symbolics.Num,
+    restriction_set_type::RestrictionSetType,
+    beta::Number,
+    lambda::Number,
+    gamma::Number,
+)
+    rest = FollowerRestrictionProblem(expr, restriction_set_type, beta, lambda, gamma)
+    push!(model.follower_restriction_problem, rest)
+
+end
+
+function SetAlpha(model::OptimizationModel, alpha::Vector)
+    #Añadir que si no es igual al length del level inf lanzar exepcion
+    model.is_alpha_zero = false
+    model._alpha = alpha
+    
+end
+
+function SetAlphaZero(model::OptimizationModel, is_alpha_zero::Bool)
+    model.is_alpha_zero = is_alpha_zero
+end
+
+function SetPoint(model::OptimizationModel,point::Dict)
+model.point=point
+end
+function GeneratorModel(is_alpha_zero::Bool)
+
+
+
+    leader = Problem([], nothing)
+
+    follower = Problem([], nothing)
+
+    return OptimizationModel(leader, Vector{LeaderRestrictionProblem}(), follower, Vector{FollowerRestrictionProblem}(), Dict(), Vector{Number}(), is_alpha_zero)
+end
+
--- def_problem/utils.jl
+++ def_problem/utils.jl
@@ -13,3 +13,19 @@ end
 
 export get_rand
 
+
+
+# Definir la función para transformar las llaves del diccionario
+function transformar_llaves(diccionario::Dict, transformacion::Function)
+    # Crear un nuevo diccionario vacío
+    nuevo_diccionario = Dict{Any, Any}()
+    
+    # Iterar sobre cada par llave-valor en el diccionario original
+    for (llave, valor) in diccionario
+        # Aplicar la transformación a la llave y añadir al nuevo diccionario
+        nueva_llave = transformacion(llave)
+        nuevo_diccionario[nueva_llave] = valor
+    end
+    
+    return nuevo_diccionario
+end

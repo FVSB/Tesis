45dccbbae741dc86223251b23be7563d639c56d9
Francisco Vicente Suárez Bellón <95106875+FVSB@users.noreply.github.com>
2025-01-18 15:12:05 -0500

    expermits todos no convexos menos el 5to

--- def_problem/utils.jl
+++ def_problem/utils.jl
@@ -23,7 +23,7 @@ export get_rand
 #Genera un vector aleatorio de dimension n
 function get_rand_vect(n)::Vector{Number}
     epsilon = 1e-10
-    return  rand(n) .+epsilon
+    return  my_round.(rand(n) .+epsilon)
  end
 
 # Definir la función para transformar las llaves del diccionario
